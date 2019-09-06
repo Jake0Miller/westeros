6cb8d64b0e091c6796c5317e975ed3e5c28d6c24
Jake Miller <jake0miller@gmail.com>
2019-09-06 10:30:10 -0600

    Create api service

---
+++ app/services/westeros_service.rb
@@ -0,0 +1,19 @@
+class WesterosService
+  def family_data(house)
+    get_json("house/#{house}")
+  end
+
+  private
+
+  def conn
+    @_conn ||= Faraday.new(url: "http://westerosapi.herokuapp.com/api/v1/") do |faraday|
+      faraday.params["api_key"] = ENV["API_KEY"]
+      faraday.adapter Faraday.default_adapter
+    end
+  end
+
+  def get_json(url)
+    response = conn.get(url)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+end

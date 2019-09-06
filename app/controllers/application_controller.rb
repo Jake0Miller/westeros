74bce6b6bcbf91b44d459c6d6c3a4006e58367cf
Jake Miller <jake0miller@gmail.com>
2019-09-06 10:13:44 -0600

    Add routes and helper method for families

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  helper_method :houses
+
+  def houses
+    [
+      ['Stark', 'stark'],
+      ['Lannister', 'lannister'],
+      ['Targaryen', 'targaryen'],
+      ['Tyrell', 'tyrell'],
+      ['Greyjoy', 'greyjoy']
+    ]
+  end
 end
---
+++ spec/features/welcome_index_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+describe 'I visit the root path' do
+  it 'I can select a house from a dropdown' do
+    visit root_path
+
+    within ".dropdown-select" do
+      select "Grejoy", :from => "Family"
+    end
+
+    click_on 'Get Members'
+
+    expect(current_path).to eq(search_path)
+    expect(current_url).to include("house=greyjoy")
+
+    expect(page).to have_content("7 Members")
+    expect(page).to have_css(".member", count: 7)
+    expect(page).to have_content(last_name)
+    expect(page).to_not have_content('Theon')
+  end
+end
